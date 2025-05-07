99928a775aab810014cddec922508c4fcaecd51e
Rajiya Sultana <rssumu04@gmail.com>
2025-05-07 22:52:08 +0600

    Added time  and score counter

---
+++ Color/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timeText;
+    private int score = 0;
+    private float time = 0f;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeText.text = Mathf.FloorToInt(time).ToString() + "s";
+
+    }
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over!");
+        
+    }
+}
